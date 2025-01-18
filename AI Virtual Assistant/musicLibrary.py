040bb18d32c8424a66804d667b6dd10acfcad630
Shraddha More <spmore2907@gmail.com>
2025-01-18 19:32:22 +0530

    Added: Virtual Assistant Files

---
+++ AI Virtual Assistant/musicLibrary.py
@@ -0,0 +1,7 @@
+
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=uq2j6RMS45A&pp=ygUKbWFyY2ggc29uZw%3D%3D",
+    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo&pp=ygUMc2t5ZmFsbCBzb25n",
+    "firstLight": "https://www.youtube.com/watch?v=0SVbJldc5wM&list=PLzCxunOM5WFIVeCUZ7bFCW7wwA_JBHsDC&index=7&pp=iAQB8AUB"   
+}
